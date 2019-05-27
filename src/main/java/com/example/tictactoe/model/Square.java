0be5b34b786e4d0025247aca8353afce51829abd
Carlos Rey Planelles <carlos.rey@itbo-ericsson.com>
2019-05-27 17:00:09 +0200

    Square model added

--- src/main/java/com/example/tictactoe/model/Square.java
+++ src/main/java/com/example/tictactoe/model/Square.java
@@ -1,5 +1,25 @@
 package com.example.tictactoe.model;
 
 public class Square {
+	
+	private String symbol;
+	private Boolean available;
+	
+	public String getSymbol() {
+		return symbol;
+	}
+	
+	public Boolean getAvailable() {
+		return available;
+	}
+	
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+	
+	public void setAvailable(Boolean available) {
+		this.available = available;
+	}
+	
 
 }
